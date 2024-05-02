eba69611129a89a5af00cd2f5d156edb658d8ad3
Lina Velandia <linakatavela22@gmail.com>
2024-05-01 19:55:36 -0500

    responsive change

---
+++ js/cactus.js
@@ -0,0 +1,49 @@
+const productDetails = [
+    {
+      name: "Garambullo ",
+      price: 24900,
+      imageUrl:"../assets/cactus1-fotor-bg-remover-20230816104226.png",
+      qty: 5,
+      heading: "Myrtillocactus Geometrizans",
+      des:
+        "Es una planta arbolada perenne carnosa, armada de espinas. Cactácea arborescente, erecta, hasta de 2- 7 m de altura, tiene forma de un candelabro, muy ramificado cerca de la base de un tronco corto, los tallos miden entre 6-12 cm de diámetro, con 6 -8 costillas, verdes azuladas, con bolas de lana de color pardo-blanquecinas en las puntas."
+    },
+    {
+      name: "Biznaga de Barril",
+      price: 7900,
+      imageUrl: "../assets/cactus2-fotor-bg-remover-2023081610597.png",
+      qty: 15,
+      heading: "Cactus Ferocactus Horridus",
+      des:
+        "Esta planta es perfecta no necesita mucho mantenimiento y puede sobrevivir largas temporadas sin necesidad de que se la riegue. Además suele estar libre de plagas y tiene una forma muy decorativa y no alcanza una gran altura. Tiene un color verde precioso y por la forma de sus costillas y las espinas la hace una planta muy estética. Pero hay que llevar cuidado con las espinas ya que son punzantes."
+    },
+    {
+      name: "Cleistocacto",
+      price: 9900,
+      imageUrl: "../assets/cactus3-fotor-bg-remover-2023081611823.png",
+      qty: 10,
+      heading: "Cleistocactus baumannii ",
+      des:
+        "Flores: diurnos, en verano, lo mayor porte de las veces tubulares, o en forma de embudo, rojos, naranjos o rosas, en individuos de, al menos, cuatro años."
+    },
+    {
+      name: "Cardón",
+      price: 10600,
+      imageUrl:
+        "../assets/Cereus_Peruvianus_Monstruosus_22o_kaleblanca_cactus_1cd99860-c9a3-433c-a70c-37f2376aaff8_1024x-fotor-bg-remover-20230816111114.png",
+      qty: 10,
+      heading: "Cereus peruvianus",
+      des:
+        "Espinas pardas y de diferente número en cada aréola.Flor blanca. Floración: en verano. Las flores se abren de noche"
+    },
+    {
+      name: "CORONA DE LECHE",
+      price: 18900,
+      imageUrl:
+        "../assets/Euphorbia-Eritrea_19o_onix_comprar-plantas-online_plantas-de-interior_cactus_1024x (1)-fotor-bg-remover-20230816111817.png",
+      qty: 15,
+      heading: "Euphorbia Eritrea",
+      des:
+        "n el caso de este cactus recomendamos que reciba luz filtrada o indirecta, si recibe sol, evita las horas centrales del día, sobretodo cuando la Euphorbia es aún jovén."
+    }
+  ];
---
+++ js/exterior.js
@@ -0,0 +1,50 @@
+
+const productDetails = [
+    {
+      name: "Buganvilla",
+      price: 24900,
+      imageUrl:"../assets/Buganvilla-Violeta_17bo_creta_plantas_con_flor_plantas_primavera_flores_1024x-fotor-bg-remover-20230816143523.png",
+      qty: 5,
+      heading: "Buganvilla Morada",
+      des:
+        "Las  Bougainvilleas o buganvillas son enredaderas de porte arbustivo compuestas de hojas y brácteas. Plantada en suelo puede alcanzar hasta 10 metros de altura por eso es una planta ideal para jardines, patios y terrazas. "
+    },
+    {
+      name: "Limonero ",
+      price: 7900,
+      imageUrl: "../assets/Limonero_22o_cocoblanca_comprar-plantas-online_plantas-de-interior_v2_1024x-fotor-bg-remover-20230816145824.png",
+      qty: 15,
+      heading: "Limonero",
+      des:
+        "El Limonero es una planta de exterior genial para dar vida a tu jardín o terraza siempre que reciba buena cantidad de sol este árbol frutal vivirá bien y dará fruto."
+    },
+    {
+      name: "Olivo",
+      price: 9900,
+      imageUrl: "../assets/Olivo_M_comprar-plantas-online_plantas-de-interior_arbol_frutal_01_1024x-fotor-bg-remover-2023081615040.png",
+      qty: 10,
+      heading: "Olivo",
+      des:
+        "El olivo es una planta de exterior que puede ubicarse en interior siempre y cuando le ofrezcas las condiciones adecuadas para prosperar y mantengas sus cuidados constantes."
+    },
+    {
+      name: "Palmera",
+      price: 10600,
+      imageUrl:
+        "../assets/Chamaerops_humilis_27o_cocoblanca_plantas_de_exterior_1024x-fotor-bg-remover-2023081615315.png",
+      qty: 10,
+      heading: "Palmito Palmera",
+      des:
+        "es una palmera arbustiva que puede llegar a medir hasta 4 metros de altura. Se trata de una planta de exterior decorativa y muy resistente. "
+    },
+    {
+      name: "Salvia Farinacea",
+      price: 18900,
+      imageUrl:
+        "../assets/Salvia-farinacea_17ao_dessieblanca_comprar-plantas-online_plantas-de-interior_1024x-fotor-bg-remover-2023081615825.png",
+      qty: 15,
+      heading: "Salvia Farinacea",
+      des:
+        "es una planta aromática que se utiliza tanto con fines culinarios como medicinales."
+    }
+  ];
