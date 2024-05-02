6127a2122d3166bfcc9b3bcaab4886539eb513b9
Lina Velandia <linakatavela22@gmail.com>
2024-05-01 19:46:20 -0500

    prueba pagina2

---
+++ js/interior.js
@@ -0,0 +1,49 @@
+const productDetails = [
+    {
+      name: "Ave del paraiso ",
+      price: 24900,
+      imageUrl:"../assets/StrelitziaAugusta24o_coco_blanca_comprar-plantas-online_plantas-de-interior_cactus_1024x-fotor-bg-remover-20230816141351.png",
+      qty: 5,
+      heading: "Strelitzia Reginae",
+      des:
+        "Luz: crece bien con sol y semi-sombra.Humedad: vive mejor con niveles de humedad altos, rocía las hojas y evita el aire seco.Riego moderado: 1 vez por semana en verano, dejar secar la capa superior de tierra entre riego y riego."
+    },
+    {
+      name: "Monstera deliciosa",
+      price: 7900,
+      imageUrl: "../assets/Monstera-Deliciosa-17o_1024x-fotor-bg-remover-20230816141956.png",
+      qty: 15,
+      heading: "Monstera deliciosa",
+      des:
+        "La costilla de Adán es una planta muy popular que se caracteriza por tener unas grandes hojas verdes partidas, como si se tratase de unas costillas. Crea unos tallos flexibles ya que en realidad es una planta trepadora que requiere soportes para mantenerse erguida."
+    },
+    {
+      name: "Maranta Lemon Lime",
+      price: 9900,
+      imageUrl: "../assets/Maranta_12o_cotton_plantas_de_exterior_8ee2d58e-eb1d-4196-9ff7-05f6161dbe08_1024x-fotor-bg-remover-20230816142326.png",
+      qty: 10,
+      heading: "Maranta Lemon Lime",
+      des:
+        "El color y dibujo de sus hojas es muy original y además si está feliz, te regalará pequeñas flores de color blanco. La Maranta Lemon Lime es la elección perfecta para aportar vida y originalidad a la decoración de tu hogar."
+    },
+    {
+      name: "Millonaria ",
+      price: 10600,
+      imageUrl:
+        "../assets/Zamioculcas_mediana_textura_1024x-fotor-bg-remover-20230816142721.png",
+      qty: 10,
+      heading: "Zamioculcas",
+      des:
+        "Luz: no necesita mucha luz, se adapta a condiciones de sombra. Poco Riego: cada 15 días. Volver a regar cuando la superficie de la tierra esté seca.Humedad: tolera los ambientes secos, no requiere pulverizaciones de follaje."
+    },
+    {
+      name: "Calathea Orbifolia",
+      price: 18900,
+      imageUrl:
+        "../assets/Calathea-Orbifolia_17o_cala_comprar-plantas-online_plantas-de-interior_1024x-fotor-bg-remover-2023081614312.png",
+      qty: 15,
+      heading: "Calathea Orbifolia",
+      des:
+        "Esta Calathea, tan difícil de encontrar, tiene hojas anchas de color verde brillante, delicadamente rayadas con un patrón azul plateado que parece casi metálico."
+  }
+]
